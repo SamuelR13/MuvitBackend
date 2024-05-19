53ea354b9b38ddbbe1d7ad73b870e304aa76c8cd
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-05-18 19:02:51 -0500

    payment method list

--- js/users/userHome/payment.js
+++ js/users/userHome/payment.js
@@ -2,56 +2,105 @@ const info = document.querySelector("#info")
 
 export function payment(userData) {
     console.log(userData)
-    info.innerHTML = `<div id="payment" class="h-100 w-100">
-    <div class="card mt-5 mb-5">
-            <form>
-                <span id="card-header">Saved cards:</span>
-                <div class="row row-1">
-                    <div class="col-2"><img class="img-fluid" src="https://img.icons8.com/color/48/000000/mastercard-logo.png"/></div>
-                    <div class="col-7">
-                        <input type="text" placeholder="**** **** **** 3193">
-                    </div>
-                    <div class="col-3 d-flex justify-content-center">
-                        <a href="#">Remove card</a>
-                    </div>
-                </div>
-                <div class="row row-1">
-                    <div class="col-2"><img  class="img-fluid" src="https://img.icons8.com/color/48/000000/visa.png"/></div>
-                    <div class="col-7">
-                        <input type="text" placeholder="**** **** **** 4296">
-                    </div>
-                    <div class="col-3 d-flex justify-content-center">
-                        <a href="#">Remove card</a>
-                    </div>
-                </div>
-                <span id="card-header">Add new card:</span>
-                <div class="row-1">
-                    <div class="row row-2">
-                        <span id="card-inner">Card holder name</span>
-                    </div>
-                    <div class="row row-2">
-                        <input type="text" placeholder="Bojan Viner">
-                    </div>
-                </div>
-                <div class="row three">
-                    <div class="col-7">
-                        <div class="row-1">
-                            <div class="row row-2">
-                                <span id="card-inner">Card number</span>
+    info.innerHTML = `
+            <div id="payment" class="h-100 w-100">
+                <div class="w-100 h-100 d-flex gap-3">
+
+                    <div class="mt-2 w-40 h-100 d-flex flex-column">
+
+                        <div class="h-10 d-flex align-items-center mb-3 clamp1 typeColor">Payment Methods</div>
+
+                        <div id="paymentList" class="h-50 w-100 d-flex flex-column gap-2 clamp3">
+                            <div id="card1" class="cardPayment h-20 w-100 d-flex align-items-center gap-2">
+                                <img  class="pe-none" height="50px" width="50px" src="../assets/img/SVG/mastercard.svg">
+                                <div class="pe-none">Mastercard</div>
+                                <div class="pe-none">****5612</div>
                             </div>
-                            <div class="row row-2">
-                                <input type="text" placeholder="5134-5264-4">
+                            <div id="card2" class="cardPayment h-20 w-100 d-flex align-items-center gap-2">
+                                <img  class="pe-none" height="50px" width="50px" src="../assets/img/SVG/visa.svg">
+                                <div class="pe-none">Visa</div>
+                                <div class="pe-none">****5612</div>
+                            </div>
+                            <div id="card3" class="cardPayment h-20 w-100 d-flex align-items-center gap-2">
+                                <img  class="pe-none" height="50px" width="50px" src="../assets/img/SVG/american-express.svg">
+                                <div class="pe-none">American Express</div>
+                                <div class="pe-none">****5612</div>
+                            </div> 
+                        </div>
+
+                        <div class="d-flex w-100 gap-3 addPaymet">
+                            <div class="d-flex align-items-center">
+                                <i class="bi bi-plus-circle clamp1"></i>
+                            </div>
+                            <div class="d-flex flex-column justify-content-between">
+                               <div class="clamp1 typeColor ">Add Payment Method</div>
+                               <div class="clamp2 text-muted">Debit / Credit Card</div>
                             </div>
                         </div>
+
                     </div>
-                    <div class="col-2">
-                        <input type="text" placeholder="Exp. date">
-                    </div>
-                    <div class="col-2">
-                        <input type="text" placeholder="CVV">
+
+                    <div id="paymentInfo" class="w-40 h-100 d-flex align-items-center">
+                        <div class="w-100 h-80 d-flex flex-column align-items-center mx-3">
+                            <div class="form-floating mb-2 w-100">
+                              <input type="text" class="form-control" id="cardName" placeholder="name">
+                              <label for="cardName">Card name (optional)</label>
+                            </div>
+                            <div class="form-floating mb-2 w-100">
+                              <input type="text" class="form-control" id="cardNumber" placeholder="Password">
+                              <label for="cardNumber">Number Card</label>
+                            </div>
+                            <div class="d-flex w-100 gap-2">
+                                <div class="form-floating w-50">
+                                    <input type="text" class="form-control" id="cardExpiry" placeholder="Password">
+                                    <label for="cardExpiry">Expiration Date</label>
+                                </div>
+                                <div class="form-floating w-50">
+                                    <input type="text" class="form-control" id="cardCVV" placeholder="Password">
+                                    <label for="cardCVV">CVV</label>
+                                </div>
+                            </div>
+                        </div>    
                     </div>
                 </div>
-                <button type="button" class="btn btn-outline-warning">Add Card</button>
-                </form>
-        </div></div>`
+            </div>`
+
+
+    const paymentList = document.querySelector("#paymentList")
+    const paymentInfo = document.querySelector("#paymentInfo")
+    paymentList.addEventListener("click", event => {
+        event.preventDefault()
+        let card = event.target
+        console.log(card);
+        paymentInfo.innerHTML = `
+        <div class="w-100 h-70 d-flex flex-column align-items-center">
+            <div class="visa-card">
+                <div class="logoContainer">
+                    <img class="svgLogo" src="../assets/img/SVG/mastercard.svg">
+                </div>
+                <div class="number-container">
+                  <label class="input-label" for="cardNumber">CARD NUMBER</label>
+                  <div class="inputstyle" id="cardNumber">**** **** **** 1234</>
+                </div>
+
+                <div class="name-date-cvv-container">
+                  <div class="name-wrapper">
+                    <label class="input-label" for="holderName">CARD HOLDER</label>
+                    <div class="inputstyle">Andrea Velez</div>
+                  </div>
+
+                  <div class="expiry-wrapper">
+                    <label class="input-label" for="expiry">VALID THRU</label>
+                    <div class="inputstyle" id="expiry">06/26</div>
+                  </div>
+                  <div class="cvv-wrapper">
+                    <label class="input-label" for="cvv">CVV</label>
+                    <div class="inputstyle" id="cvv">***</div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <button id="cancel_service" type="button" class="btn btn-outline-danger clamp2 mt-3">Remove Card</button>        
+        `
+    })
 }
